b94074e0b8c9884e48b4da712eb18aeaf8e43572
Kristina <kriska2000@abv.bg>
2020-06-18 22:15:28 +0300

    100/100

--- DOM Manipulations/Exercise/04. Locked-Profile/app.js
+++ DOM Manipulations/Exercise/04. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
+    let allButtons = Array.from(document.getElementsByTagName('button'));
     
+    for (const button of allButtons) {
+        button.addEventListener('click', show);
+    }
+
+    function show(event){
+        let radioButtonLock = event.target.parentElement.children[2];
+        let hiddenDiv = event.target.parentElement.children[9];
+        if(radioButtonLock.checked === false){
+            if(event.target.textContent === "Show more"){
+                hiddenDiv.style.display = "block";
+                event.target.textContent = "Hide it";
+            }
+            else{
+                hiddenDiv.style.display = "none";
+                event.target.textContent = "Show more";
+            }
+        }
+    }
 }
--- DOM Manipulations/Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulations/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,30 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let allButtons = Array.from(document.getElementsByTagName('button'));
+    let buttonEncode = allButtons[0];
+    let buttonDecode = allButtons[1];
+
+    buttonEncode.addEventListener('click', encode);
+    buttonDecode.addEventListener('click', decode);
+
+    function encode(event) {
+        let text = document.getElementsByTagName('textarea')[0].value;
+        let charCodes = [];
+
+        for (let i = 0; i < text.length; i++) {
+            charCodes.push(text.charCodeAt(i) + 1);
+        }
+        let decodeText = String.fromCharCode(...charCodes);
+        document.getElementsByTagName('textarea')[0].value = '';
+        document.getElementsByTagName('textarea')[1].value = decodeText;
+    }
+
+    function decode(event) {
+        let text = document.getElementsByTagName('textarea')[1].value;
+        let charCodes = [];
+        for (let i = 0; i < text.length; i++) {
+            charCodes.push(text.charCodeAt(i) - 1);
+        }
+        let encodeText = String.fromCharCode(...charCodes);
+        document.getElementsByTagName('textarea')[1].value = encodeText;
+    }
 }
