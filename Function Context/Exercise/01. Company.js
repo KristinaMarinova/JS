4e772765abe08f4b369538e6000a3b016ee86fb9
Kristina <kriska2000@abv.bg>
2020-06-22 12:03:16 +0300

    100/100

--- Function Context/Exercise/01. Company.js
+++ Function Context/Exercise/01. Company.js
@@ -1,59 +1,67 @@
-class Company{
-
-    constructor(){
+class Company {
+    constructor() {
         this.departments = [];
     }
 
-    addEmployee(username, salary, position, department){
-
-        let arg = Array.from(arguments);
-
-        if(arg.some(a => a === null || a === "undefined" || a === "")){
-            throw new Error("Invalid input!");           
+    addEmployee(username, salary, position, department) {
+        this._validateParam(username);
+        this._validateParam(salary);
+        this._validateParam(position);
+        this._validateParam(department);
+        if (salary < 0) {
+            throw new Error('Invalid input!');
         }
 
-        if(salary < 0){
-            throw new Error("Invalid input!");           
-        }
-        
-        let employee = {
-            username: username,
-            salary: salary,
-            position: position,
-            department: department
-        };
-
-        if(this.departments.some(d => d.name === department)){
-            this.departments.find(d => d.name === department).employees.push(employee);
-        }
-        else{
-            let dep = {
+        let current = this.departments.find(d => d.name === department);
+        if (current === undefined) {
+            current = {
                 name: department,
-                employees: [employee],
-            }
-
-            this.departments.push(dep);
+                employees: []
+            };
+            this.departments.push(current);
         }
-
-        return `New employee is hired. Name: ${username}. Position: ${position}`
-    }
-
-    bestDepartment(){
-        let depsMap = this.departments.map(d => ({ name: d.name, avg: (d.employees.map(e => e.salary).reduce((a, b) => a + b)) / d.employees.length}));
-        let dep = depsMap.sort((a, b) => b.avg - a.avg)[0];
-
-        let result = [];
-        result.push(`Best Department is: ${dep.name}`);
-        result.push(`Average salary: ${dep.avg.toFixed(2)}`);
-
-        this.departments.find(d => d.name === dep.name).employees.sort((a, b) => b.salary - a.salary || a.username.localeCompare(b.username));
-        let department = this.departments.find(d => d.name === dep.name);
-        department.employees.forEach(element => {
-            result.push(`${element.username} ${element.salary} ${element.position}`)
+        current.employees.push({
+            username,
+            salary,
+            position
         });
 
-        return result.join("\n");
+        return `New employee is hired. Name: ${username}. Position: ${position}`;
     }
+
+    bestDepartment() {
+        const departments = this.departments.map(d => {
+            const dep = {
+                name: d.name,
+                employees: d.employees.slice(),
+            };
+            let total = 0;
+            for (let employee of dep.employees) {
+                total += employee.salary;
+            }
+            dep.averageSalary = total / dep.employees.length;
+            return dep;
+        });
+        departments.sort((a, b) => b.averageSalary - a.averageSalary);
+
+        const best = departments[0];
+        if (best !== undefined) {
+            best.employees.sort((a, b) => b.salary - a.salary || a.username.localeCompare(b.username));
+            const result = [
+                `Best Department is: ${best.name}`,
+                `Average salary: ${best.averageSalary.toFixed(2)}`,
+            ];
+            best.employees.forEach(e => result.push(`${e.username} ${e.salary} ${e.position}`));
+            return result.join('\n');
+        }
+    }
+
+    _validateParam(param) {
+        if (param === '' || param === undefined || param === null) {
+            throw new Error('Invalid input!');
+        }
+    }
+
 }
 
 let c = new Company();
