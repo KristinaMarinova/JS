a8b98ae8fd713f75e5efda35fb163a8d5118f571
Kristina <kriska2000@abv.bg>
2020-07-15 00:44:37 +0300

    Asynchronous Programming

--- JS Applications/Asynchronous Programming/Exercise/02. FISHER-GAME/app.js
+++ JS Applications/Asynchronous Programming/Exercise/02. FISHER-GAME/app.js
@@ -1,6 +1,15 @@
-function attachEvents() {
-    console.log('TODO...');
-}
+import Request from '/Request.js';
+import View from '/View.js';
+import { addFormData, clearFormData, getBtns, getCatchId, methods } from '/Res.js';
 
-attachEvents();
+const attachBtnListeners = () => getBtns().map(e => e.addEventListener('click', handleBtnClick));
+const handleBtnClick = async e => {
+  const elem = e.target;
+  const method = methods[elem.className];
+  const data = elem.getAttribute('data-id') || addFormData();
+  const res = await Request(getCatchId(e), method, data);
+  View(res, method, getCatchId(e), clearFormData, addFormData());
+  attachBtnListeners();
+};
 
+attachBtnListeners();
