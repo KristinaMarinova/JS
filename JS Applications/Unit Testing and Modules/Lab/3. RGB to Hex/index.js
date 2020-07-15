30bc0ca8f6e3116a4211bd870c3f4d82d2ad8c2c
Kristina <kriska2000@abv.bg>
2020-07-15 23:39:44 +0300

    Unit Testing and Modules

--- JS Applications/Unit Testing and Modules/Lab/3. RGB to Hex/index.js
+++ JS Applications/Unit Testing and Modules/Lab/3. RGB to Hex/index.js
@@ -1,10 +1,10 @@
 function rgbToHexColor(red, green, blue) {
     if (!Number.isInteger(red) || (red < 0) || (red > 255))
-        return undefined; // Red value is invalid
+        return undefined; 
     if (!Number.isInteger(green) || (green < 0) || (green > 255))
-        return undefined; // Green value is invalid
+        return undefined; 
     if (!Number.isInteger(blue) || (blue < 0) || (blue > 255))
-        return undefined; // Blue value is invalid
+        return undefined; 
     return "#" +
         ("0" + red.toString(16).toUpperCase()).slice(-2) +
         ("0" + green.toString(16).toUpperCase()).slice(-2) +
