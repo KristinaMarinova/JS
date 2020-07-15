e2187412976fecc3dc982676c36a7c7f13984d8e
Kristina <kriska2000@abv.bg>
2020-07-15 23:41:58 +0300

    Unit Testing and Modules

--- JS Applications/Unit Testing and Modules/Lab/4. Add Subtract/test.js
+++ JS Applications/Unit Testing and Modules/Lab/4. Add Subtract/test.js
@@ -8,39 +8,39 @@ describe("createCalculator", function(){
         instance = new createCalculator();
     });
         
-    it("should return correct result with number parameter", function(){
+    it("should return correct result with number param", function(){
         instance.add(5);
         let actual = instance.get();
         expect(actual).to.be.equal(5);
     })
-    it("should return correct result with number parameter", function(){
+    it("should return correct result with number param", function(){
         instance.subtract(5);
         let actual = instance.get();
         expect(actual).to.be.equal(-5);
     })
 
-    it("should return correct result with number parameter as string", function(){
+    it("should return correct result with number param as string", function(){
         instance.add("5");
         let actual = instance.get();
         expect(actual).to.be.equal(5);
     })
-    it("should return correct result with number parameter as string", function(){
+    it("should return correct result with number param as string", function(){
         instance.subtract("5");
         let actual = instance.get();
         expect(actual).to.be.equal(-5);
     })
 
-    it("should return number with number parameter", function(){
+    it("should return number with number param", function(){
         instance.subtract(10);
         let actual = instance.get();
         expect(typeof actual).to.be.equal("number");
     })
     
-    it("should return NaN with non-number parameter", function(){
+    it("should return NaN with non-number param", function(){
         instance.subtract("aaa");
         assert.equal(isNaN(instance.get()), true);
     })
-    it("should return NaN with non-number parameter", function(){
+    it("should return NaN with non-number param", function(){
         instance.add("aaa");
         assert.equal(isNaN(instance.get()), true);
     })
