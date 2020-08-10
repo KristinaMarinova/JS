ba12b3e891894aa986a254d190092928bb41ff65
Kristina <kriska2000@abv.bg>
2020-08-10 15:44:21 +0300

    finish second task

---
+++ JS Advanced/Exams/JS Advanced Exam - 27 June 2020/02. Pet House.js
@@ -0,0 +1,113 @@
+function solveClasses() {
+
+    class Pet {
+        constructor(owner, name) {
+            this.owner = owner;
+            this.name = name;
+            this.comments = [];
+        }
+
+        addComment(comment) {
+            if (this.comments.includes(comment)) {
+                throw new Error(`This comment is already added!`);
+            }
+
+            this.comments.push(comment);
+            return `Comment is added.`;
+        }
+
+        feed() {
+            return `${this.name} is fed`;
+        }
+
+        toString() {
+            const result = [`Here is ${this.owner}'s pet ${this.name}.`];
+
+            if (this.comments.length > 0) {
+                result.push(`Special requirements: ${this.comments.join(', ')} `);
+            }
+
+            return result.join('\n');
+        }
+    }
+
+
+    class Cat extends Pet {
+        constructor(owner, name, insideHabits, scratching) {
+            super(owner, name);
+            this.insideHabits = insideHabits;
+            this.scratching = scratching;
+        }
+
+        feed() {
+            const result = [super.feed()];
+            result.push(`happy and purring.`);
+            return result.join(', ');
+        }
+
+        toString() {
+            const result = [super.toString()];
+            result.push(`Main information:`);
+
+            if (this.scratching === false) {
+                result.push(`${this.name} is a cat with ${this.insideHabits}`);
+            }
+            else {
+                result.push(`${this.name} is a cat with ${this.insideHabits}, but beware of scratches.`);
+            }
+            return result.join('\n');
+        }
+    }
+
+
+    class Dog extends Pet {
+        constructor(owner, name, runningNeeds, trainability) {
+            super(owner, name);
+            this.runningNeeds = runningNeeds;
+            this.trainability = trainability;
+        }
+
+        feed() {
+            const result = [super.feed()];
+            result.push(`happy and wagging tail.`);
+            return result.join(', ');
+        }
+
+        toString() {
+            const result = [super.toString()];
+            result.push(`Main information:`);
+            result.push(`${this.name} is a dog with need of ${this.runningNeeds}km running every day and ${this.trainability} trainability.`);
+            return result.join('\n');
+        }
+    }
+
+
+
+
+    return {
+        Pet,
+        Cat,
+        Dog
+    }
+
+}
+
+
+let classes = solveClasses();
+let pet = new classes.Pet('Ann', 'Merry');
+console.log(pet.addComment('likes bananas'));
+console.log(pet.addComment('likes sweets'));
+console.log(pet.feed());
+console.log(pet.toString());
+
+let cat = new classes.Cat('Jim', 'Sherry', 'very good habits', true);
+console.log(cat.addComment('likes to be brushed'));
+console.log(cat.addComment('sleeps a lot'));
+console.log(cat.feed());
+console.log(cat.toString());
+
+let dog = new classes.Dog('Susan', 'Max', 5, 'good');        
+console.log(dog.addComment('likes to be brushed'));
+console.log(dog.addComment('sleeps a lot'));
+console.log(dog.feed());
+console.log(dog.toString());
