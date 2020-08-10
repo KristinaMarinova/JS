0d2e8d55ef59b65b4c8041f8ce0fa6c533369741
Kristina <kriska2000@abv.bg>
2020-08-10 19:40:49 +0300

    complete exam without third task

--- JS Advanced/Exams/JS Advanced - 23 February 2020/02. Article/02. Article.js
+++ JS Advanced/Exams/JS Advanced - 23 February 2020/02. Article/02. Article.js
@@ -1,11 +1,11 @@
 class Article {
-    constructor(title, creator) {
+    constructor(title, creator){
         this.title = title;
         this.creator = creator;
         this._comments = [];
         this._likes = [];
-
     }
+
     get likes (){
 
         if(this._likes.length === 0){
@@ -18,6 +18,7 @@ class Article {
 
         return `${this._likes[0]} and ${this._likes.length - 1} others like this article!`;
     }
+
     like (username){
 
         if(this._likes.some(x => x === username)){
@@ -31,35 +32,40 @@ class Article {
         this._likes.push(username);
         return `${username} liked ${this.title}!`;
     }
-    dislike(username) {
-        if (!this._likes.some(x => x === username)) {
+
+    dislike (username){
+        if(!this._likes.some(x => x === username)){
             throw new Error("You can't dislike this article!");
         }
+
+        this._likes = this._likes.filter(x => x !== username);
+
         return `${username} disliked ${this.title}`;
     }
-    comment(username, content, id) {
+
+    comment (username, content, id){
         let comment = this._comments.find(c => c.id === id);
 
-        if (comment === undefined) { 
+        if(comment === undefined){
             let newComment = {
-                id: this._comments.length + 1,
-                username: username,
-                content: content,
-                replies: []
+                id: this._comments.length + 1, 
+                username: username, 
+                content: content, 
+                replies:[]
             }
             this._comments.push(newComment);
             return `${username} commented on ${this.title}`;
         }
 
         let reply = {
-            id: `${comment.id}.${comment.replies.length + 1}`,
-            username: username,
+            id: `${comment.id}.${comment.replies.length + 1}`, 
+            username: username, 
             content: content
         }
         comment.replies.push(reply);
         return "You replied successfully";
-
     }
+
     toString(sortingType){
         let result = [
             `Title: ${this.title}`,
@@ -81,32 +87,15 @@ class Article {
         return result.join("\n");
     }
 
-    customSort(array, sortingType) {
-        if (sortingType === 'asc') {
+    customSort(array, sortingType){
+        if(sortingType === "asc"){
             return array.sort((a, b) => a.id - b.id);
         }
-        if (sortingType === 'desc') {
+        if(sortingType === "desc"){
             return array.sort((a, b) => b.id - a.id);
-        } 
-        if (sortingType === 'username') {
+        }
+        if(sortingType === "username"){
             return array.sort((a, b) => a.username.localeCompare(b.username));
         }
     }
 }
-
-
-let art = new Article("My Article", "Anny");
-art.like("John");
-console.log(art.likes);
-art.dislike("John");
-console.log(art.likes);
-art.comment("Sammy", "Some Content");
-console.log(art.comment("Ammy", "New Content"));
-art.comment("Zane", "Reply", 1);
-art.comment("Jessy", "Nice :)");
-console.log(art.comment("SAmmy", "Reply@", 1));
-console.log()
-console.log(art.toString('username'));
-console.log()
-art.like("Zane");
-console.log(art.toString('desc'));
--- JS Advanced/Exams/JS Advanced - 23 February 2020/03. Info Parser/tests.js
+++
@@ -1,5 +0,0 @@
-let Parser = require("./solution.js");
-let assert = require("chai").assert;
-describe("MyTests", () => {
-    //ToDO
-});
