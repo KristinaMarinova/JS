258ee42b5ff23359952c992620725424e6221faa
Kristina <kriska2000@abv.bg>
2020-08-11 07:24:30 +0300

    all tasks from exam are completed

--- JS Advanced/Exams/JS Advanced Exam Preparation May 2020/03. Bank.js
+++ JS Advanced/Exams/JS Advanced Exam Preparation May 2020/03. Bank.js
@@ -70,30 +70,3 @@ class Bank {
     }
 
 }
-
-let bank = new Bank('SoftUni Bank');
-
-console.log(bank.newCustomer({firstName: 'Svetlin', lastName: 'Nakov', personalId: 6233267}));
-console.log(bank.newCustomer({firstName: 'Mihaela', lastName: 'Mileva', personalId: 4151596}));
-
-bank.depositMoney(6233267, 250);
-console.log(bank.depositMoney(6233267, 250));
-bank.depositMoney(4151596,555);
-
-console.log(bank.withdrawMoney(6233267, 125));
-
-console.log(bank.customerInfo(6233267));
-
-// expected output
-// { firstName: ‘Svetlin’, lastName: ‘Nakov’, personalId: 6233267 } 
-// { firstName: ‘Mihaela’, lastName: ‘Mileva’, personalId: 4151596 }
-// 500$
-// 375$
-// Bank name: SoftUni Bank
-// Customer name: Svetlin Nakov
-// Customer ID: 6233267
-// Total Money: 375$
-// Transactions:
-// 3. Svetlin Nakov withdrew 125$!
-// 2. Svetlin Nakov made depostit of 250$!
-// 1. Svetlin Nakov made depostit of 250$!
