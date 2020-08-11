ed8e33ac9b9a8e345db1e02ff4a4fb532696c1be
Kristina <kriska2000@abv.bg>
2020-08-11 07:23:45 +0300

    all tasks from exam is completed

--- JS Advanced/Exams/JS Advanced Exam Preparation May 2020/03. Bank.js
+++ JS Advanced/Exams/JS Advanced Exam Preparation May 2020/03. Bank.js
@@ -1,83 +1,80 @@
-class Bank{
-    constructor(bankName){
+class Bank {
+    constructor(bankName) {
         this._bankName = bankName;
         this.allCustomers = [];
     }
 
-    
-    newCustomer(customer){
-        if(this.allCustomers.some(c => c.personalId === customer.personalId)){
-            throw new Error(`${customer.firstName} ${customer.lastName} is already our customer!”`)
+    newCustomer({ firstName, lastName, personalId }) {
+        if (this.allCustomers.some(c => c.personalId === personalId)) {
+            throw new Error(`${firstName} ${lastName} is already our customer!`);
         }
 
-        customer.totalMoney = 0;
-        customer.allTransactions = [];
-        this.allCustomers.push(customer);
-        return {
-            firstName: customer.firstName,
-            lastName: customer.lastName,
-            personalId: customer.personalId,
-        };
+        this.allCustomers.push({
+            firstName,
+            lastName,
+            personalId,
+            totalMoney: 0,
+            transactions: []
+        });
+
+        return { firstName, lastName, personalId };
     }
 
-    depositMoney (personalId, amount){
-        let customer = this.allCustomers.find(c => c.personalId === personalId);
+    depositMoney(personalId, amount) {
+        const customer = this.allCustomers.find(c => c.personalId === personalId);
 
-        if(customer === undefined){
-            throw new Error("We have no customer with this ID!");
+        if (customer === undefined) {
+            throw new Error(`We have no customer with this ID!`);
         }
 
-        customer["totalMoney"] += amount;
-        customer.allTransactions.push(`${customer.firstName} ${customer.lastName} made deposit of ${amount}$!`)
+        customer.totalMoney += amount;
+        customer.transactions.push(`${customer.firstName} ${customer.lastName} made deposit of ${amount}$!`);
+
         return `${customer.totalMoney}$`;
     }
 
-    withdrawMoney (personalId, amount){
-        let customer = this.allCustomers.find(c => c.personalId === personalId);
+    withdrawMoney(personalId, amount) {
+        const customer = this.allCustomers.find(c => c.personalId === personalId);
 
-        if(customer === undefined){
-            throw new Error("We have no customer with this ID!");
+        if (customer === undefined) {
+            throw new Error(`We have no customer with this ID!`);
         }
 
-        if(customer.totalMoney < amount){
-            throw new Error(`${customer.firstName} ${customer.lastName} does not have enough money to withdraw that amount!`);
+        if (customer.totalMoney < amount) {
+            throw new Error(`${custmoer.firstName} ${customer.lastName} does not have enough money to withdraw that amount!`);
         }
 
         customer.totalMoney -= amount;
-        customer.allTransactions.push(`${customer.firstName} ${customer.lastName} withdrew ${amount}$!`)
+        customer.transactions.push(`${customer.firstName} ${customer.lastName} withdrew ${amount}$!`);
 
         return `${customer.totalMoney}$`;
     }
 
-    customerInfo (personalId){
-        let customer = this.allCustomers.find(c => c.personalId === personalId);
+    customerInfo(personalId) {
+        const customer = this.allCustomers.find(c => c.personalId === personalId);
 
-        if(customer === undefined){
-            throw new Error("We have no customer with this ID!");
+        if (customer === undefined) {
+            throw new Error(`We have no customer with this ID!`);
         }
 
-        let result = [
-            `Bank name: ${this._bankName}`,
-            `Customer name: ${customer.firstName} ${customer.lastName}`,
-            `Customer ID: ${customer.personalId}`,
-            `Total Money: ${customer.totalMoney}$`,
-            `Transactions:`
-        ]
+        let result = `Bank name: ${this._bankName}\nCustomer name: ${customer.firstName} ${customer.lastName}\nCustomer ID: ${customer.personalId}\nTotal Money: ${customer.totalMoney}$\n`;
+        let transactions = [];
 
-        for (let index = customer.allTransactions.length; index > 0; index--){
-            const element = customer.allTransactions[index-1];
-            result.push(`${index}. ${element}`);
+       for (let i = customer.transactions.length - 1; i >= 0; i--) {
+            transactions.push(`${i + 1}. ` + customer.transactions[i]);
         }
 
-        return result.join("\n");
+        result += transactions.join('\n');
 
+        return result.trim();
     }
+
 }
 
-let bank = new Bank("SoftUni Bank");
+let bank = new Bank('SoftUni Bank');
 
-console.log(bank.newCustomer({firstName: "Svetlin", lastName: "Nakov", personalId: 6233267}));
-console.log(bank.newCustomer({firstName: "Mihaela", lastName: "Mileva", personalId: 4151596}));
+console.log(bank.newCustomer({firstName: 'Svetlin', lastName: 'Nakov', personalId: 6233267}));
+console.log(bank.newCustomer({firstName: 'Mihaela', lastName: 'Mileva', personalId: 4151596}));
 
 bank.depositMoney(6233267, 250);
 console.log(bank.depositMoney(6233267, 250));
@@ -86,3 +83,17 @@ bank.depositMoney(4151596,555);
 console.log(bank.withdrawMoney(6233267, 125));
 
 console.log(bank.customerInfo(6233267));
+
+// expected output
+// { firstName: ‘Svetlin’, lastName: ‘Nakov’, personalId: 6233267 } 
+// { firstName: ‘Mihaela’, lastName: ‘Mileva’, personalId: 4151596 }
+// 500$
+// 375$
+// Bank name: SoftUni Bank
+// Customer name: Svetlin Nakov
+// Customer ID: 6233267
+// Total Money: 375$
+// Transactions:
+// 3. Svetlin Nakov withdrew 125$!
+// 2. Svetlin Nakov made depostit of 250$!
+// 1. Svetlin Nakov made depostit of 250$!
